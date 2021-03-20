0b5503b385853d61cbd93e669f595e2e4c6e9e40
Airscript <francesco@airscript.it>
2021-03-20 16:12:40 +0000

    feat: add filter model

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -3,9 +3,21 @@ from django.contrib import admin
 
 # Importing: Models.
 from .models import Car
+from .models import Filter
+
+# Admin Class for Model Car.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('make','model','year', 'engine')
+    search_fields = ['model']
+
+# Admin Class for Model Filter.
+class FilterAdmin(admin.ModelAdmin):
+    list_display = ('code', 'typology', 'quantity')
+    search_fields = ['code']
 
 # Registering models.
-admin.site.register(Car)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Filter, FilterAdmin)
 
 # Overriding text.
 admin.site.site_header = "Morazyne"
--- warehouse/models.py
+++ warehouse/models.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 # Declaring: Models.
+
+# Declaring Car Model.
 class Car(models.Model):
   make = models.CharField(max_length=100, verbose_name="Produttore")
   model = models.CharField(max_length=100, verbose_name="Modello")
@@ -12,9 +14,24 @@ class Car(models.Model):
   engine = models.CharField(max_length=10, choices=[('petrol', 'Benzina'), ('diesel', 'Diesel')], default="petrol", verbose_name="Motore")
 
   def __str__(self):
-   return 'Auto: ' + self.model
+    # pylint: disable=E1101
+    return self.make + " " + self.model + " (" + self.get_engine_display() + ", " + self.year + ")" 
 
   class Meta:
     verbose_name = _("Auto")
     verbose_name_plural = _("Auto")
     unique_together = ("make", "model", "engine")
+
+# Declaring Filter Model.
+class Filter(models.Model): 
+  code = models.CharField(max_length=16, verbose_name="Codice")
+  typology = models.CharField(max_length=100, verbose_name="Tipologia")
+  quantity = models.PositiveIntegerField(default=0, verbose_name="Quantita'")
+  car = models.ForeignKey(Car, verbose_name="Auto", on_delete=models.CASCADE, blank=True, null=True)
+
+  def __str__(self):
+   return 'Filtro: ' + self.code + " " + self.typology
+  
+  class Meta:
+    verbose_name = _("Filtro")
+    verbose_name_plural = _("Filtri")
