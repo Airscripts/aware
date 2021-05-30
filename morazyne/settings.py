fd0f8ebc9fc77ab87275f61f3f1d0d1d9b7bdd66
Airscript <francesco@airscript.it>
2021-05-30 14:37:32 -0400

    refactor: drop sqlite support in favour of postgres

--- morazyne/settings.py
+++ morazyne/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'morazyne.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'morazyne',
+        'USER': 'admin',
+        'PASSWORD': 'Morazyne2021',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
