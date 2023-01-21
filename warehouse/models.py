21c362dd67c87110ae80f3828e66683d61c79813
Airscript <francesco@airscript.it>
2023-01-21 23:28:38 +0100

    refactor: clean up models.py

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,11 +1,9 @@
-# Importing: Dependencies.
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Declaring: Choices.
 
 CYLINDERS_CHOICES = [
   ('900', '900'),
@@ -30,14 +28,30 @@ OIL_SPECIFIC_CHOICES = [
   ('RN17', 'RN17'),
 ]
 
-# Declaring: Models.
 
-# Declaring Filter Model.
 class Filter(models.Model): 
   id = models.AutoField(primary_key=True)
   code = models.CharField(max_length=16, verbose_name="Codice")
-  primary = models.CharField(choices=PRIMARY_CHOICES, verbose_name="Primario", max_length=4, default="No")
-  typology = models.CharField(max_length=32, verbose_name="Tipologia", choices=[('air', 'Aria'), ('oil', 'Olio'), ('passenger', 'Abitacolo'), ('diesel', 'Gasolio'), ('gas', 'Gas')])
+
+  primary = models.CharField(
+    choices=PRIMARY_CHOICES,
+    verbose_name="Primario",
+    max_length=4,
+    default="No"
+  )
+
+  typology = models.CharField(
+    max_length=32,
+    verbose_name="Tipologia",
+    choices=[
+      ("air", "Aria"),
+      ("oil", "Olio"),
+      ("passenger", "Abitacolo"),
+      ("diesel", "Gasolio"),
+      ("gas", "Gas")
+    ]
+  )
+
   quantity = models.PositiveIntegerField(default=0, verbose_name="Quantita'")
   cars = models.ManyToManyField("Car", through="CarFilter", verbose_name="Auto")
 
@@ -49,27 +63,61 @@ class Filter(models.Model):
     verbose_name = _("Filtro")
     verbose_name_plural = _("Filtri")
 
-# Declaring Car Model.
+
 class Car(models.Model):
   id = models.AutoField(primary_key=True)
-  make = models.CharField(max_length=100, verbose_name="Produttore")
-  model = models.CharField(max_length=100, verbose_name="Modello")
   year = models.CharField(max_length=4, verbose_name="Anno")
-  engine = models.CharField(max_length=10, choices=[('petrol', 'Benzina'), ('diesel', 'Diesel')], default="petrol", verbose_name="Motore")
-  cylinders = models.CharField(choices=CYLINDERS_CHOICES, verbose_name="Cilindrata", max_length=16)
-  oil_quantity = models.FloatField(default=0, verbose_name="Quantita' Olio", validators=[MinValueValidator(0), MaxValueValidator(10000)])
-  oil_specific = models.CharField(choices=OIL_SPECIFIC_CHOICES, default="RN0710", verbose_name="Specifica Olio", max_length=16)
-  filters = models.ManyToManyField("Filter", through="CarFilter" , verbose_name="Filtri")
+  model = models.CharField(max_length=100, verbose_name="Modello")
+  make = models.CharField(max_length=100, verbose_name="Produttore")
+
+  engine = models.CharField(
+    max_length=10,
+    default="petrol",
+    verbose_name="Motore",
+    choices=[('petrol', 'Benzina'), ('diesel', 'Diesel')],
+  )
+
+  cylinders = models.CharField(
+    max_length=16,
+    choices=CYLINDERS_CHOICES,
+    verbose_name="Cilindrata",
+  )
+
+  oil_quantity = models.FloatField(
+    default=0,
+    MaxValueValidator(10000)],
+    verbose_name="Quantita' Olio",
+    validators=[MinValueValidator(0),
+  )
+
+  oil_specific = models.CharField(
+    max_length=16,
+    default="RN0710",
+    choices=OIL_SPECIFIC_CHOICES,
+    verbose_name="Specifica Olio",
+  )
+
+  filters = models.ManyToManyField(
+    "Filter",
+    through="CarFilter",
+    verbose_name="Filtri"
+  )
 
   def __str__(self):
     # pylint: disable=E1101
-    return self.make + " " + self.model + " (" + self.get_engine_display() + ", " + self.year + ")" 
+    return f"{self.make} {self.model} ({self.get_engine_display()}, {self.year})"
 
   class Meta:
     verbose_name = _("Auto")
     verbose_name_plural = _("Auto")
     unique_together = ("make", "model", "engine")
 
+
 class CarFilter(models.Model):
   car = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name="Auto")
-  filter = models.ForeignKey(Filter, on_delete=models.CASCADE, verbose_name="Filtro")
+
+  filter = models.ForeignKey(
+    Filter,
+    on_delete=models.CASCADE,
+    verbose_name="Filtro"
+  )
